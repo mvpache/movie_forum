80ac6e186acebf7855529e8b55496a293e3436fc
mvpache <mvpache@msn.com>
2018-08-24 21:17:26 -0700

    Added server import

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const path = require ('path');
+const path = require('path');
+
+const server = require('./server/server');
 
 server.use(express.static(path.join(__dirname, 'client/build')));
 
