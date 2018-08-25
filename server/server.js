dff687f19ab0c83153958e6bbee4f893b87f0043
mvpache <mvpache@msn.com>
2018-08-24 21:24:08 -0700

    Added cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 
 const server = express();
 
+const originUrl = process.env.NODE_ENV === 'production'
+  ? 'https:movieramble.herokuapp.com'
+  : 'http://localhost:3000';
+
+const corsOptions = {
+  origin: originUrl,
+  credentials: true,
+  methods: ['GET', 'PUT', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+server.use(cors(corsOptions));
 server.use(express.json());
 
 module.exports = server;
